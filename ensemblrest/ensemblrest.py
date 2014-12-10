973d0286dd71b92c769820c527069189a64f716b
Paolo Cozzi <paolo.cozzi@tecnoparco.org>
2014-12-10 17:27:11 +0100

    IMPORTANT: replaced '?' in '"' in LICENSE.rst. Added license reminder in sources
    As reported in http://www.gnu.org/licenses/gpl-howto.html, each source
    file in the whole project needs to have this text
    
        This file is part of Nome-Programma.
    
        Nome-Programma is free software: you can redistribute it and/or modify
        it under the terms of the GNU General Public License as published by
        the Free Software Foundation, either version 3 of the License, or
        (at your option) any later version.
    
        Nome-Programma is distributed in the hope that it will be useful,
        but WITHOUT ANY WARRANTY; without even the implied warranty of
        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
        GNU General Public License for more details.
    
        You should have received a copy of the GNU General Public License
        along with Nome-Programma.  If not, see <http://www.gnu.org/licenses/>.
    
    Where 'Nome-Programma' was set to project name. I have replaced the
    ? symbol with " from LICENSE.rst (maybe could be an encoding issue?)

--- ensemblrest/ensemblrest.py
+++ ensemblrest/ensemblrest.py
@@ -1,10 +1,27 @@
 """
+
+    This file is part of pyEnsemblRest.
+
+    pyEnsemblRest is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    pyEnsemblRest is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with pyEnsemblRest.  If not, see <http://www.gnu.org/licenses/>.
+
 	EnsemblRest is a library for Python that wrap the EnsEMBL REST API.
 	It simplifies all the API endpoints by abstracting them away from the
 	end user and thus also ensures that an amendments to the library and/or
 	EnsEMBL REST API won't cause major problems to the end user.
 
 	Any questions, comments or issues can be addressed to gawbul@gmail.com.
+	
 """
 
 # import system modules
