bd0408d090ef3efbe51d932f6b4337de4f5201d3
Steve Moss <gawbul@gmail.com>
2024-11-28 20:01:19 +0000

    Use metadata version for __version__

--- pyensemblrest/__init__.py
+++ pyensemblrest/__init__.py
@@ -1,8 +1,10 @@
+import importlib.metadata
+
 __author__ = "Steve Moss"
 __copyright__ = "Copyright (C) 2013-2024, Steve Moss"
 __credits__ = ["Steve Moss"]
 __license__ = "GNU GPLv3"
-__version__ = "0.0.0"
+__version__: str = importlib.metadata.version("pyensemblrest")
 __maintainer__ = "Steve Moss"
 __email__ = "gawbul@gmail.com"
 __status__ = "beta"
